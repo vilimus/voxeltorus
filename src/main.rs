b478f7fbe3866e7956eaba62839d90e497fba7fb
vilimus <35790256+vilimus@users.noreply.github.com>
2023-12-13 22:32:21 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,245 +1,303 @@
-
-// voxel raycasting in rust
-
-use macroquad::prelude::*;
-use nalgebra::Vector3;
 use std::f32::consts::PI;
-
-// universal constants
-const WORLDSIZE: [usize; 3] = [16, 64, 16];
-const LX: usize = WORLDSIZE[0];
-const LY: usize = WORLDSIZE[1];
-const LZ: usize = WORLDSIZE[2];
-
-const SCREEN_SIZE: (f32, f32) = (640.0, 480.0);
-const SCREEN: (usize, usize) = (64, 48);
-
-const VIEW_DISTANCE: usize = 32;
-const BREAK_DISTANCE: usize = 4;
-const FOV: (f32, f32) = (PI/2.0, PI/2.0*(SCREEN.1 as f32)/(SCREEN.0 as f32));
-
+use macroquad::prelude::*;
+use macroquad::rand::{rand, gen_range};
+use rayon::prelude::*;
+// Settings
+const RESOLUTION: (f32, f32) = (800 as f32, 600 as f32);
+const SCREEN: (usize, usize) = (400, 250);
+const WORLDSIZE: [usize; 3] = [256, 256, 256];
 const MOVEMENT_SPEED: f32 = 0.5;
-const ROT_SPEED: (f32, f32) = (0.5, 0.5);
-const AMBIENT: [f32; 3] = [0.75, 0.75, 1.0];
-
-const RECT_SIZE_X: f32 = SCREEN_SIZE.0 / (SCREEN.0 as f32);
-const RECT_SIZE_Y: f32 = SCREEN_SIZE.1 / (SCREEN.1 as f32);
-
+const ROTATION_SPEED: (f32, f32) = (0.5, 0.5);
+const FOV: (f32, f32) = (PI/2.0, PI/2.0*(SCREEN.1 as f32)/(SCREEN.0 as f32));
+const VIEW_DISTANCE: usize = 128;
+const TOUCH_DISTANCE: usize = 16;
+const AMBIENT: Vec4 = vec4(1.0, 1.0, 1.0, 1.0);
+const RECTSIZE_X: f32 = RESOLUTION.0 / (SCREEN.0 as f32);
+const RECTSIZE_Y: f32 = RESOLUTION.1 / (SCREEN.1 as f32);
+struct Camera {
+	i: usize,
+	position: Vec3,
+	angle: Vec2,
+	movement_speed: f32,
+	rotation_speed: (f32, f32),
+	fov: (f32, f32),
+	screen: (usize, usize),
+}
 #[derive(Copy, Clone)]
-struct Cell {
-    visible: bool,
-    color: Vector3<f32>
+struct Voxel {
+	color: Vec4,
+	brightness: f32,
 }
-
-
-fn load_world() -> Vec<Vec<Vec<Cell>>> {
-
-    let air   = Cell {visible: false,  color: Vector3::new(0.0, 0.0, 0.0)};
-    let stone = Cell {visible: true,   color: Vector3::new(0.1, 0.1, 0.1)};
-    let dirt  = Cell {visible: true,   color: Vector3::new(0.2, 0.1, 0.0)};
-    let grass = Cell {visible: true,   color: Vector3::new(0.0, 0.5, 0.0)};
-    let sky   = Cell {visible: true,   color: Vector3::new(0.5, 0.5, 1.0)};
-    let wood  = Cell {visible: true,   color: Vector3::new(0.5, 0.25, 0.0)};
-    let leaves = Cell {visible: true,   color: Vector3::new(0.0, 0.25, 0.0)};
-
-    let mut world: Vec<Vec<Vec<Cell>>> = vec![vec![vec![air; LZ]; LY]; LX];
-
-    for i in 0..LX {
-        for k in 0..LZ {
-            for j in 0..44 {
-                world[i][j][k] = stone;
-            }
-            for j in 44..47 {
-                world[i][j][k] = dirt;
-            }
-            world[i][47][k] = grass;
-            world[i][63][k] = sky;
-        }
-    }
-    for i in 22..11 {
-        for j in 6..11 {
-            world[i][50][j] = leaves;
-            world[i][51][j] = leaves;
-        }
-    }
-    for i in 7..10 {
-        for j in 7..10 {
-            world[i][52][j] = leaves;
-            world[i][53][j] = leaves;
-        }
-    }
-    world[8][54][8] = leaves;
-    for i in 48..54 {
-        world[8][i][8] = wood;
-    }
-    return world;
+type Neighbors = [usize; 6];
+type World = Vec<(Voxel, Neighbors)>;
+fn furl(i: usize, j: usize, k: usize, ny: usize, nz: usize) -> usize {
+	return i*ny*nz + j*nz + k
 }
-
-fn lattice_intersect(pos: Vector3<f32>, v: Vector3<f32>) -> (Vector3<f32>, Vector3<i32>) {
-    let t = Vector3::new(
-        ((v.x.signum() + 1.0) / 2.0 - pos.x) / v.x,
-        ((v.y.signum() + 1.0) / 2.0 - pos.y) / v.y,
-        ((v.z.signum() + 1.0) / 2.0 - pos.z) / v.z,
-    );
-    let mut i_min: usize = 0;
-    let t_min: f32 = t.min();
-    for i in 0..3 {
-        if t[i] == t_min {
-            i_min = i
-        }
-    }
-    let t_star = t[i_min];
-    let mut key = Vector3::new(0, 0, 0);
-    key[i_min] = v[i_min].signum() as i32;
-    let key2 = key.map(|x| x as f32);
-    let x_new = pos + t_star*v - key2;
-    return (x_new, key);
+fn randf() -> f32 {
+	(rand() as f32) / (u32::MAX as f32)
 }
-
-fn raycast(world: &Vec<Vec<Vec<Cell>>>, cell_x0: Vector3<usize>, x0: Vector3<f32>, v: Vector3<f32>, max_steps: usize) -> (Vector3<usize>, usize) {
-    let mut cell_x = cell_x0;
-    let mut x = x0;
-    let mut steps = 0;
-    let mut key = Vector3::new(0, 0, 0);
-    for _ in 0..max_steps {
-        (x, key) = lattice_intersect(x, v);
-        cell_x.x = ((cell_x.x as i32) + key.x).rem_euclid(LX as i32) as usize;
-        cell_x.y = ((cell_x.y as i32) + key.y).rem_euclid(LY as i32) as usize;
-        cell_x.z = ((cell_x.z as i32) + key.z).rem_euclid(LZ as i32) as usize;
-        if world[cell_x.x][cell_x.y][cell_x.z].visible == true {
-            break;
-        }
-        steps = steps + 1;
-    }
-    return (cell_x, steps)
+fn randr(a: f32, b: f32) -> f32 {
+	a + (b -a) * randf()
 }
-
-#[macroquad::main("voxeltorusrs")]
+fn combine_worlds(w1: World, w2: World) -> World {
+	// add them as lists
+	return w1;
+}
+fn build_world(nx: usize, ny: usize, nz: usize) -> World {
+	let voxel = Voxel {
+		color: vec4(0.0, 0.0, 0.0, 0.0),
+		brightness: 0.0,
+	};
+	let mut world: World = vec![(voxel, [0, 0, 0, 0, 0, 0]); nx*ny*nz];
+	for i in 0..nx {
+		for j in 0..ny {
+			for k in 0..nz {
+				let n = furl(i, j, k, ny, nz);
+				world[n].1 = [
+					furl((i as i32 + 1).rem_euclid(nx as i32) as usize, j, k, ny, nz),
+					furl((i as i32 - 1).rem_euclid(nx as i32) as usize, j, k, ny, nz),
+					furl(i, (j as i32 + 1).rem_euclid(ny as i32) as usize, k, ny, nz),
+					furl(i, (j as i32 - 1).rem_euclid(ny as i32) as usize, k, ny, nz),
+					furl(i, j, (k as i32 + 1).rem_euclid(nz as i32) as usize, ny, nz),
+					furl(i, j, (k as i32 - 1).rem_euclid(nz as i32) as usize, ny, nz)
+				];
+			}
+		}
+	}
+	for i in 0..nx {
+		for j in 0..16 {
+			for k in 0..nz {
+				let n = furl(i, j, k, ny, nz);
+				world[n].0.color = vec4(
+					randr(0.3, 0.4),
+					randr(0.8, 0.9),
+					randr(0.3, 0.4),
+					gen_range(0, 2) as f32
+				);
+			}
+		}
+	}
+	for _ in 0..200 {
+		let i = gen_range(0, world.len());
+		if world[i].0.color.w == 1.0 {
+			world[i].0.color = vec4(1.0, 1.0, 1.0, 1.0);
+			world[i].0.brightness = 1.0;
+		}
+	}
+	return world;
+}
+fn lattice_intersect(pos: Vec3, v: Vec3) -> (Vec3, [i32; 3], f32) {
+	let t = ((v.signum() + 1.0) / 2.0 - pos) / v;
+	let t_min: f32 = t.min_element();
+	let mut i_min: usize = 0;
+	for i in 0..3 {
+		if t[i] == t_min {
+			i_min = i
+		}
+	}
+	let mut key: [i32; 3] = [0, 0, 0];
+	key[i_min] = v[i_min].signum() as i32;
+	let key2 = vec3(key[0] as f32, key[1] as f32, key[2] as f32);
+	let x_new = pos + t_min*v - key2;
+	return (x_new, key, (t_min*v).length());
+}
+fn raycast(world: &World, vox_id: usize, basepoint: Vec3, ray: Vec3, max_steps: usize) -> (usize, Vec3, f32) {
+	let (mut i, mut x) = (vox_id, basepoint);
+	let mut k: [i32; 3];
+	let mut dist = 0.0;
+	let mut dt = 0.0;
+	for step in 0..max_steps {
+		(x, k, dt)  = lattice_intersect(x, ray);
+		dist = dist + dt;
+		if k[0] == 1 {
+			i = world[i].1[0];
+		} else if k[0] == -1 {
+			i = world[i].1[1];
+		} else if k[1] == 1 {
+			i = world[i].1[2];
+		} else if k[1] == -1 {
+			i = world[i].1[3];
+		} else if k[2] == 1 {
+			i = world[i].1[4];
+		} else if k[2] == -1 {
+			i = world[i].1[5];
+		}
+		if world[i].0.color.w == 1.0 {
+			return (i, x, dist);
+		}
+	}
+	return (i, x, max_steps as f32);
+}
+fn update(world: &mut World, delta_t: f32) {
+	for _ in 0..((delta_t * (world.len() as f32)) as usize)  {
+		let i = gen_range(0, world.len() - 1);
+		let mut neighbor_sum = vec4(0.0, 0.0, 0.0, 0.0);
+		for j in world[i].1 {
+			neighbor_sum = neighbor_sum + world[j].0.color;
+		}
+		if world[i].0.color.w == 1.0 && 4.0 <= neighbor_sum.w && neighbor_sum.w <= 6.0 {
+			world[i].0.color.w = 1.0;
+		} else if world[i].0.color.w < 1.0 && 3.0 <= neighbor_sum.w && neighbor_sum.w <= 6.0 {
+			world[i].0.color.w = 1.0;
+		} else {
+			world[i].0.color.w = 0.0;
+		}
+	}
+}
+fn update_brightness(world: &mut World) {
+	for i in 0..world.len() {
+		if world[i].0.color.w < 1.0 {
+			let mut neighbor_sum = 0.0;
+			for j in world[i].1 {
+				if world[j].0.brightness > neighbor_sum {
+					neighbor_sum = world[j].0.brightness;
+				}
+			}
+			world[i].0.brightness = neighbor_sum - 0.02;
+			if world[i].0.brightness < 0.0 {
+				world[i].0.brightness = 0.0;
+			}
+		}
+	}
+}
+#[macroquad::main("VoxelTorus")]
 async fn main() {
-    request_new_screen_size(SCREEN_SIZE.0, SCREEN_SIZE.1);
-
-    let mut world = load_world();
-
-    let mut cell: Vector3<usize> = Vector3::new(0, 48, 0);
-    let mut pos: Vector3<f32> = Vector3::new(0.5, 0.5, 0.5); // these two should essentially be in a pair..
-
-    let mut cam: [f32; 2] = [0.0, 0.0]; // camera angles, horizontal/vertical rotation
-
-    let mut look : Vector3<f32> = Vector3::new(1.0, 0.0, 0.0);
-    let mut up   : Vector3<f32> = Vector3::new(0.0, 1.0, 0.0);
-    let mut right: Vector3<f32> = Vector3::new(0.0, 0.0, 1.0);
-
-    set_cursor_grab(true);
-    show_mouse(false);
-
-    let rect_size_x = SCREEN_SIZE.0 / (SCREEN.0 as f32);
-    let rect_size_y = SCREEN_SIZE.1 / (SCREEN.1 as f32);
-
-    let mut alive = true;
-	let mut fps = 0.0;
-    while alive {
-        if is_key_pressed(KeyCode::Escape) {
-            alive = false;
-        }
-
-        let mut vel = Vector3::new(0.0, 0.0, 0.0);
-        if is_key_down(KeyCode::E) {
-            vel.y = vel.y + 1.0
-        }
-        if is_key_down(KeyCode::Q) {
-            vel.y = vel.y - 1.0
-        }
-        if is_key_down(KeyCode::W) {
-            vel = vel + look;
-        }
-        if is_key_down(KeyCode::S) {
-            vel = vel - look;
-        }
-        if is_key_down(KeyCode::A) {
-            vel = vel - right;
-        }
-        if is_key_down(KeyCode::D) {
-            vel = vel + right;
-        }
-		if is_key_down(KeyCode::I) {
-			println!("fps: {}", fps);
-        }
-        let vel_norm = vel.norm();
-        if vel_norm > 0.0 {
-            pos = pos + MOVEMENT_SPEED * vel / vel.norm();
-        }
-        if pos.x < 0.0 {
-            cell.x = (((cell.x as i32) - 1).rem_euclid(LX as i32)) as usize;
-            pos.x = pos.x + 1.0;
-        }
-        if pos.y < 0.0 {
-            cell.y = (((cell.y as i32) - 1).rem_euclid(LY as i32)) as usize;
-            pos.y = pos.y + 1.0;
-        }
-        if pos.z < 0.0 {
-            cell.z = (((cell.z as i32) - 1).rem_euclid(LZ as i32)) as usize;
-            pos.z = pos.z + 1.0;
-        }
-        if pos.x > 1.0 {
-            cell.x = (((cell.x as i32) + 1).rem_euclid(LX as i32)) as usize;
-            pos.x = pos.x - 1.0;
-        }
-        if pos.y > 1.0 {
-            cell.y = (((cell.y as i32) + 1).rem_euclid(LY as i32)) as usize;
-            pos.y = pos.y - 1.0;
-        }
-        if pos.z > 1.0 {
-            cell.z = (((cell.z as i32) + 1).rem_euclid(LZ as i32)) as usize;
-            pos.z = pos.z - 1.0;
-        }
-
-        if is_key_down(KeyCode::Left) {
-            cam[0] = cam[0] - ROT_SPEED.0;
-        }
-        if is_key_down(KeyCode::Right) {
-            cam[0] = cam[0] + ROT_SPEED.0;
-        }
-        if is_key_down(KeyCode::Up) {
-            cam[1] = cam[1] + ROT_SPEED.1;
-        }
-        if is_key_down(KeyCode::Down) {
-            cam[1] = cam[1] - ROT_SPEED.1;
-        }
-
-        let mouse_delta = mouse_delta_position();
-        cam[0] = cam[0] - ROT_SPEED.0 * mouse_delta.x;
-        cam[1] = cam[1] + ROT_SPEED.1 * mouse_delta.y;
-
-        look  = Vector3::new( cam[0].cos()*cam[1].cos(), cam[1].sin(),  cam[0].sin()*cam[1].cos());
-        up    = Vector3::new(-cam[0].cos()*cam[1].sin(), cam[1].cos(), -cam[0].sin()*cam[1].sin());
-        right = Vector3::new(-cam[0].sin(),              0.0,           cam[0].cos());
-
-        if is_mouse_button_pressed(MouseButton::Left) {
-            let (cell_x, steps) = raycast(&world, cell, pos, look, BREAK_DISTANCE);
-            world[cell_x.x][cell_x.y][cell_x.z].visible = false;
-        }
-        for i in 0..(SCREEN.0) {
-            for j in 0..(SCREEN.1) {
-                let a_i = (((i as f32) / (SCREEN.0 as f32) - 0.5) * FOV.0).atan();
-                let b_j = (((j as f32) / (SCREEN.1 as f32) - 0.5) * FOV.1).atan();
-                let look_ij = look + a_i*right - b_j*up;
-
-                let (cell_x, steps) = raycast(&world, cell, pos, look_ij, VIEW_DISTANCE);
-
-                let cell_hit = world[cell_x.x][cell_x.y][cell_x.z];
-                let fade: f32 = 1.0 - (steps as f32) / (VIEW_DISTANCE as f32);
-
-                let color = Color::new(
-                    cell_hit.color.x * fade + AMBIENT[0] * (1.0 - fade),
-                    cell_hit.color.y * fade + AMBIENT[1] * (1.0 - fade),
-                    cell_hit.color.z * fade + AMBIENT[2] * (1.0 - fade),
-                    1.0
-                );
-                draw_rectangle(RECT_SIZE_X*(i as f32), RECT_SIZE_Y*(j as f32), RECT_SIZE_X, RECT_SIZE_Y, color)
-            }
-        }
-        next_frame().await;
-        fps = 1.0/get_frame_time();
-        //println!("FPS: {}", fps);
-    }
+	request_new_screen_size(RESOLUTION.0, RESOLUTION.1);
+	let mut world = build_world(WORLDSIZE[0], WORLDSIZE[1], WORLDSIZE[2]);
+	//update(&mut world, 10.0);
+	//for _ in 0..20 {
+	//	update_brightness(&mut world);
+	//}
+	next_frame().await;
+	let mut camera = Camera {
+		i: 0,
+		position: vec3(0.5, 0.5, 0.5),
+		angle: vec2(0.0, 0.0),
+		movement_speed: MOVEMENT_SPEED,
+		rotation_speed: ROTATION_SPEED,
+		fov: FOV,
+		screen: SCREEN,
+	};
+	let mut screen: Vec<Vec<Vec4>> = vec![vec![vec4(0.0, 0.0, 0.0, 0.0); camera.screen.1]; camera.screen.0];
+	let mut grabbed = true;
+	while world[camera.i].0.color[3] == 1.0 {
+		camera.i = world[camera.i].1[2];
+	}
+	let mut selected = Voxel {
+		color: vec4(1.0, 1.0, 1.0, 1.0),
+		brightness: 0.8,
+	};
+	loop {
+		if is_mouse_button_released(MouseButton::Left) {
+			grabbed = true;
+		}
+		if is_key_down(KeyCode::Escape) {
+			grabbed = false;
+		}
+		set_cursor_grab(grabbed);
+		show_mouse(!grabbed);
+		let mut mouse_delta = vec2(0.0, 0.0);
+		if grabbed {
+			mouse_delta = mouse_delta_position();
+		}
+		camera.angle = camera.angle - vec2(camera.rotation_speed.0 * mouse_delta.x, -camera.rotation_speed.1 * mouse_delta.y);
+		camera.angle[1] = clamp(camera.angle[1], -PI/2.0, PI/2.0);
+		let look  = vec3( camera.angle[0].cos()*camera.angle[1].cos(), camera.angle[1].sin(),  camera.angle[0].sin()*camera.angle[1].cos());
+		let up	= vec3(-camera.angle[0].cos()*camera.angle[1].sin(), camera.angle[1].cos(), -camera.angle[0].sin()*camera.angle[1].sin());
+		let right = vec3(-camera.angle[0].sin(),					   0.0,					camera.angle[0].cos());
+		let mut dx = vec3(0.0, 0.0, 0.0);
+		if is_key_down(KeyCode::E) {
+			dx = dx + vec3(0.0, 1.0, 0.0)
+		}
+		if is_key_down(KeyCode::Q) {
+			dx = dx - vec3(0.0, 1.0, 0.0)
+		}
+		if is_key_down(KeyCode::W) {
+			dx = dx + look;
+		}
+		if is_key_down(KeyCode::S) {
+			dx = dx - look;
+		}
+		if is_key_down(KeyCode::A) {
+			dx = dx - right;
+		}
+		if is_key_down(KeyCode::D) {
+			dx = dx + right;
+		}
+		match dx.try_normalize() {
+			Some(dx) => {
+				camera.position = camera.position + camera.movement_speed * dx;
+			},
+			None => {},
+		}
+		let neighbors = world[camera.i].1;
+		let mut camera_delta = vec3(0.0, 0.0, 0.0);
+		if camera.position[0] < 0.0 {
+			camera.i = neighbors[1];
+			camera_delta[0] = 1.0;
+		} else if camera.position[0] > 1.0 {
+			camera.i = neighbors[0];
+			camera_delta[0] = -1.0;
+		} else if camera.position[1] < 0.0 {
+			camera.i = neighbors[3];
+			camera_delta[1] = 1.0;
+		} else if camera.position[1] > 1.0 {
+			camera.i = neighbors[2];
+			camera_delta[1] = -1.0;
+		} else if camera.position[2] < 0.0 {
+			camera.i = neighbors[5];
+			camera_delta[2] = 1.0;
+		} else if camera.position[2] > 1.0 {
+			camera.i = neighbors[4];
+			camera_delta[2] = -1.0;
+		}
+		camera.position = camera.position + camera_delta;
+		let (target_i, target_x, _) = raycast(&world, camera.i, camera.position, look, TOUCH_DISTANCE);
+		if is_mouse_button_pressed(MouseButton::Left) {
+			world[target_i].0.color.w = 0.0;
+		}
+		if is_mouse_button_pressed(MouseButton::Middle) {
+			if world[target_i].0.color.w == 1.0 {
+				selected = world[target_i].0;
+			}
+		}
+		if is_mouse_button_pressed(MouseButton::Right) {
+			if world[target_i].0.color.w == 1.0 {
+				let (i, _, _) = raycast(&world, target_i, target_x, -look, 1);
+				world[i].0.color = selected.color;
+				world[i].0.brightness = selected.brightness;
+			}
+		}
+		// update_brightness(&mut world);
+		screen.par_iter_mut().enumerate().for_each(|(i, row)| {
+			row.par_iter_mut().enumerate().for_each(|(j, cell)| {
+				let right_coeff = (((i as f32) / (camera.screen.0 as f32) - 0.5) * camera.fov.0).atan();
+				let up_coeff = (((j as f32) / (camera.screen.1 as f32) - 0.5) * camera.fov.1).atan();
+				let ray = look + right_coeff*right - up_coeff*up;
+				let (rayhit_i, rayhit_x, distance) = raycast(&world, camera.i, camera.position, ray, VIEW_DISTANCE);
+				let mut fade = 0.0;
+				let mut fade = 1.7321 * distance / (VIEW_DISTANCE as f32);// * 3.0.pow(-1.0/3.0);
+				if rayhit_i == target_i {
+					fade = 0.5*(fade + 1.0);
+				}
+				let (shortstop_i, _, _) = raycast(&world, rayhit_i, rayhit_x, -ray, 1);
+				let local_brightness = clamp(world[shortstop_i].0.brightness, 0.0, 1.0);
+				let color = (1.0 - local_brightness)*fade*AMBIENT + local_brightness*(1.0 - fade)*world[rayhit_i].0.color;
+				*cell = color;
+			})
+		});
+		screen.iter().enumerate().for_each(|(i, row)| {
+			row.iter().enumerate().for_each(|(j, _)| {
+				draw_rectangle(
+					RECTSIZE_X*(i as f32),
+					RECTSIZE_Y*(j as f32),
+					RECTSIZE_X,
+					RECTSIZE_Y,
+					Color::from_vec(screen[i][j])
+				);
+			})
+		});
+		draw_text(&format!("VoxelTorus {}", (1.0 / get_frame_time()) as usize), 2.0, 16.0, 24.0, WHITE);
+		next_frame().await;
+	}
 }
